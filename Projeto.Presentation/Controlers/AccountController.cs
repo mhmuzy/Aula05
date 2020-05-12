ffdb3e75482e285b11cd4f4242ae051130317196
marcio henrique <mhmuzy@embratel.com.br>
2020-05-12 10:23:09 -0300

    Treinamento

--- Projeto.Presentation/Controlers/AccountController.cs
+++ Projeto.Presentation/Controlers/AccountController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Projeto.Presentation.Areas.AreaRestrita.Models;
 
 namespace Projeto.Presentation.Controlers
 {
@@ -12,5 +16,40 @@ namespace Projeto.Presentation.Controlers
         {
             return View();
         }
+
+        //[HttpPost]
+        //public IActionResult Login(LoginModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            if (model.Email.Equals("sergio.coti@gmail.com") && model.Senha.Equals("123456")) //provisório
+        //            {
+        //                //criando a identificação de acesso..
+        //                var identity = new ClaimsIdentity(
+        //                    new []
+        //                    {
+        //                        new Claim(ClaimTypes.Name, model.Email),
+        //                        //nome de usuário
+        //                        new Claim(ClaimTypes.Role, "ADMINISTRADOR")
+        //                        //perfil
+        //                    },
+        //                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+        //                //gravar em cookie
+        //                var principal = new ClaimsPrincipal(identity);
+        //                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+        //            }
+        //        }
+        //        catch (Exception)
+        //        {
+
+        //            throw;
+        //        }
+
+                
+        //    }
+        //}
     }
 }
---
+++ Projeto.Presentation/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.Presentation.Areas.AreaRestrita.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Por favor, informe o email.")]
+        [EmailAddress(ErrorMessage = "Endereço de email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a senha.")]
+        public string Senha { get; set; }
+    }
+}
